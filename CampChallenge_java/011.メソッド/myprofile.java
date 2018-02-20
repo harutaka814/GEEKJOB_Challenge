506af067b669b525e20f7ae0b63dc760cadd68cb
FU-HARUTAKA\晴嵩 <harutaka814@gmail.com>
2018-02-20 13:34:01 +0900

    JAVA　メソッド　ユーザー定義のメソッド作成　課題提出します。

---
+++ CampChallenge_java/011.メソッド/myprofile.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaPractice3;
+
+/**
+ *
+ * @author 晴嵩
+ */
+public class myprofile {
+       
+        public static void myprofile(){
+            System.out.println("私の名前は幾戸晴嵩です。");
+            System.out.println("1994年8月14日生まれです");
+            System.out.println("よろしくお願いします。");
+    }
+       public static void main(String[] args){
+           for(int i=0;i<10;){
+               i++;
+               System.out.println(i);
+               myprofile();
+           }
+           
+           
+       }
+         
+}
