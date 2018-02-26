68cb45c6d38c02580e80a66ee0f5f111dcc45b30
FU-HARUTAKA\晴嵩 <harutaka814@gmail.com>
2018-02-26 23:21:27 +0900

    JAVA　標準クラス　文字列3　課題提出します。

---
+++ CampChallenge_java/015.標準クラス/Moji3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaPractice5;
+
+/**
+ *
+ * @author 晴嵩
+ */
+public class Moji3 {
+    public static void main(String[] args){
+        String s = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        s = s.replace("I","い");
+        s = s.replace("U","う");
+        System.out.println(s);
+    }
+    
+}
