c737c8f3e25d49e3324713227a46da59096b8458
FU-HARUTAKA\晴嵩 <harutaka814@gmail.com>
2018-02-26 23:21:15 +0900

    JAVA　標準クラス　文字列1　課題提出します。

---
+++ CampChallenge_java/015.標準クラス/Moji1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaPractice5;
+
+/**
+ *
+ * @author 晴嵩
+ */
+public class Moji1 {
+    public static void main(String[] args){
+        String myName = "Harutaka Kido";
+        System.out.println("名前のバイト数:" + myName.getBytes().length);
+        System.out.println("名前の文字数:" + myName.length());
+    }
+    
+}
