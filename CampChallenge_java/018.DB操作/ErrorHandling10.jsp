f3457dd06c82528b941bd877385e98886bc3b6e4
FU-HARUTAKA\晴嵩 <harutaka814@gmail.com>
2018-03-06 18:50:22 +0900

    JAVA　DB操作　特定のレコードの削除をするフォーム作成　課題提出します。

---
+++ CampChallenge_java/018.DB操作/ErrorHandling10.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : ErrorHandling10
+    Created on : 2018/03/06, 17:52:11
+    Author     : 晴嵩
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>ID削除フォーム</title>
+    </head>
+    <body>
+        <form action="/javaWebPractice2/errorHandling10" method="post">
+            IDを入力してください<br>
+            ID:<input type="text" name="ID"><br>
+            <input type="submit" value="送信">
+            
+    </body>
+</html>
---
+++ CampChallenge_java/018.DB操作/errorHandling10.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+
+/**
+ *
+ * @author 晴嵩
+ */
+@WebServlet(urlPatterns = {"/errorHandling10"})
+public class errorHandling10 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            
+            //リクエストの取得
+            request.setCharacterEncoding("UTF-8");
+            String ID = request.getParameter("ID");
+            
+            //リクエストパラメータのチェック
+            String errorMsg = "";
+            if((ID == null) || (ID.length() == 0)){
+                errorMsg += "IDが入力されていません";
+            }
+             if(errorMsg.length() != 0){
+                out.println(errorMsg);
+            }else{
+            //データベースへの接続
+            Connection db_con = null;
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                db_con = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/GEEKJOB","root","");
+                
+                //SQL文の作成
+                String sql = "DELETE FROM profiles WHERE profilesID=" + ID +";";
+                PreparedStatement db_st = db_con.prepareStatement(sql);
+                db_st.executeUpdate();
+                
+                }catch(SQLException e){
+                    out.println(e.getMessage());
+                }catch(ClassNotFoundException e){
+                    out.println(e.getMessage());
+                }finally{
+                    try{
+                        if(db_con != null){
+                            db_con.close();
+                        }
+                    }catch(SQLException e){
+                        out.println(e.getMessage());
+                    }
+                }
+            }
+             out.println("ID:" + ID + "のデータを削除しました");
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            //out.println("<title>Servlet errorHandling10</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            //out.println("<h1>Servlet errorHandling10 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
